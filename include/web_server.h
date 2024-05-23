6acabb78e9e614940c905491e3c2d4ad538060a0
tayswi13 <1575138548@qq.com>
2024-05-23 16:34:18 +0800

    建立了epoll处理事件的框架

--- include/web_server.h
+++ include/web_server.h
@@ -4,19 +4,32 @@
 
 #include <iostream>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <cstring>
 #include <cassert>
+#include <sys/epoll.h>
+#include <unistd.h>
+
+const int MAX_EVENT_NUMBER = 10000; // 最大事件数
 
 class WebServer {
 public:
     WebServer();
     ~WebServer();
 
-    void init(int port);
+    void init(int port);    // 初始化
     void event_listen();    // 进行监听
+    void event_loop();      // 启动服务器
+    bool deal_with_conn();    // 处理客户端连接请求
+    bool deal_with_signal();    // 处理信号
+    void deal_with_read(int sockfd);    // 处理读事件
+    void deal_with_write(int sockfd);    // 处理写事件
 private:
     int m_port; // 监听端口
     int m_listenfd; // 监听fd
+    int m_epollfd;  // epoll监听树fd
+    int m_pipefd[2];    // 一对相互连接的套接字描述符
+    struct epoll_event events[MAX_EVENT_NUMBER];    // 作为epoll_wait的传出参数
 };
 
 #endif
--- src/web_server.cpp
+++ src/web_server.cpp
@@ -5,7 +5,10 @@ WebServer::WebServer() {
 }
 
 WebServer::~WebServer() {
-
+    close(m_listenfd);
+    close(m_epollfd);
+    close(m_pipefd[0]);
+    close(m_pipefd[1]);
 }
 
 void WebServer::init(int port) {
@@ -16,6 +19,7 @@ void WebServer::event_listen() {
     int ret; // 检查返回值
     struct sockaddr_in addr;    // 监听地址结构
 
+    // 初始化监听描述符
     m_listenfd = socket(AF_INET, SOCK_STREAM, 0);
     assert(m_listenfd >= 0);
 
@@ -29,4 +33,72 @@ void WebServer::event_listen() {
 
     ret = listen(m_listenfd, 20);
     assert(ret == 0);
+
+    // epoll部分
+    struct epoll_event ev;  // 用于设置lfd的属性
+    m_epollfd = epoll_create(1024);
+    assert(m_epollfd != -1);
+
+    ev.events = EPOLLIN;
+    ev.data.fd = m_listenfd;
+    ret = epoll_ctl(m_epollfd, EPOLL_CTL_ADD, m_listenfd, &ev); // 将lfd挂上监听树
+    assert(ret != -1);
+
+    ret = socketpair(AF_UNIX, SOCK_STREAM, 0, m_pipefd);    // 同一台机器上的两个进程之间建立全双工的通信管道
+    assert(ret != -1);
+
+}
+
+void WebServer::event_loop() {
+    bool stop_server = false;
+
+    while(!stop_server) {
+        int number = epoll_wait(m_epollfd, events, MAX_EVENT_NUMBER, -1);
+        assert(number != -1);
+
+        for(int i = 0; i < number; i++) {
+            int sockfd = events[i].data.fd;
+
+            // 对处于就绪状态的fd进行处理
+            if(sockfd == m_listenfd) {  
+                // 有客户端的连接请求
+                bool flag  = deal_with_conn();
+                if(flag == false)
+                    continue;
+            }
+            else if(events[i].events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR)) {
+                // 服务器端关闭连接
+            }
+            else if((sockfd == m_pipefd[0]) && (events[i].events & EPOLLIN)) {
+                // 处理信号
+                bool flag = deal_with_signal();
+                if(flag == false)
+                    break;  // 报错一下
+            }
+            else if(events[i].events & EPOLLIN) {
+                // 处理客户端发来的数据
+                deal_with_read(sockfd);
+            }
+            else if(events[i].events & EPOLLOUT) {
+                // 向客户端发送数据
+                deal_with_write(sockfd);
+            }
+        }
+    }
+}
+
+bool WebServer::deal_with_conn() {
+
+}
+
+bool WebServer::deal_with_signal() {
+
+}
+
+void WebServer::deal_with_read(int sockfd) {
+
+}
+
+void WebServer::deal_with_write(int sockfd) {
+
 }
